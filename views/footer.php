cf24d85971d333f9801a2c8afdd59fe5b0ab8143
Oussama EL BOUENANI <oussama.elbouenani@student.vinci.be>
2020-09-22 19:12:54 +0200

    edit - footer

--- views/footer.php
+++ views/footer.php
@@ -8,6 +8,7 @@
 			<p id="copyright">Mise en page d'après
 				<a href="http://www.alsacreations.com/tutoriels/">Alsacréations</a>
 			</p>
+			<p id="author">Edité par Oussama EL BOUENANI</p>
 		</div><!-- #pied -->
 	</div><!-- #global -->
 	</body>
